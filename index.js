412f34eee4276408cda1b7177ee70f732135280d
Sachin <mittalsam98@gmail.com>
2020-03-20 13:48:03 +0530

    final code

---
+++ index.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config()
+var cron = require('node-cron');
+
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+function sendMessage(){
+
+    client.messages
+    .create({
+        body: 'Hi you have just won a precise smile type yor message here http://thesachin.in/',
+        from: '+12057724706',
+        to: '+919050909461'
+    })
+    .then(message => console.log(message))
+    .catch(err=> console.log("Some error is occured - "+err));
+
+}
+
+cron.schedule('* * * * *', () => {
+    sendMessage();
+    console.log('Message sent');
+  });
